def41b9cb024452b7a0d108a5b6f8b837b6fe81a
Unknown <jeremy.fievet@outlook.fr>
2017-03-29 14:11:25 +0200

    update

--- appyTroc/app/src/main/java/fr/lille/iut/appytroc/MainActivity.java
+++ appyTroc/app/src/main/java/fr/lille/iut/appytroc/MainActivity.java
@@ -7,11 +7,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    Button createUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        createUser = (Button) findViewById(R.id.createAccount);
+//
+//        createUser.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                Intent intent = new Intent(MainActivity.this)
+//
+//            }
+//        });
         setContentView(R.layout.activity_main);
     }
 }
